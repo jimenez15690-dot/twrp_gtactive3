b4472a8cf70a38d96f05e0b40b7b99f2e965b314
jimenez15690-dot <jimenez15690@googlemail.com>
2026-09-12 22:46:48 +0200

    Update twrp_gtactive3.mk

--- twrp_gtactive3.mk
+++ twrp_gtactive3.mk
@@ -1,11 +1,11 @@
+PRODUCT_NAME := twrp_gtactive3
+PRODUCT_DEVICE := gtactive3
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := Galaxy Tab Active3
+PRODUCT_MANUFACTURER := samsung
+
 # Inherit from the common Open Source product configuration
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
 # Inherit from TWRP product configuration
 $(call inherit-product, vendor/twrp/config/common.mk)
-
-PRODUCT_DEVICE := gtactive3
-PRODUCT_NAME := twrp_gtactive3
-PRODUCT_MODEL := Galaxy Tab Active3
-PRODUCT_BRAND := samsung
-PRODUCT_MANUFACTURER := samsung
