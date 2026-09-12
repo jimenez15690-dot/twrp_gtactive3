7486ff4ab0008af58bdd4ca4c55f07251ac588ab
jimenez15690-dot <jimenez15690@googlemail.com>
2026-09-12 20:51:02 +0200

    Update product configuration inheritance for TWRP

--- twrp_gtactive3.mk
+++ twrp_gtactive3.mk
@@ -1,8 +1,8 @@
 # Inherit from the common Open Source product configuration
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
-# Inherit from our custom product configuration
-$(call inherit-product, vendor/omni/config/common.mk)
+# Inherit from TWRP product configuration
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 PRODUCT_DEVICE := gtactive3
 PRODUCT_NAME := twrp_gtactive3
