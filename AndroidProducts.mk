e03ea928cfaedf375bab35e1043fb2e00d7a0608
jimenez15690-dot <jimenez15690@googlemail.com>
2026-09-12 20:41:35 +0200

    Update AndroidProducts.mk for new product configurations

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,1 +1,9 @@
-PRODUCT_MAKEFILES := $(LOCAL_DIR)/twrp_gtactive3.mk
+LOCAL_DIR := $(call my-dir)
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/twrp_gtactive3.mk
+
+COMMON_LUNCH_CHOICES := \
+    twrp_gtactive3-eng \
+    twrp_gtactive3-userdeb
+ug
